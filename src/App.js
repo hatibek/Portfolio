1e04f40266fbebeef24a2219b24720fb40d2cf65
Hatibe Kabulantok <hatibekara@gmail.com>
2021-05-23 14:42:01 -0700

    Creates links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+const links = ['About', 'Projects', 'Experience', 'Education'];
 
 function App() {
   return (
-    <div className="App">
-      <header className="navbar">
-        <span className="name">
-        HATIBE KABULANTOK
-        </span>
-      </header>
-      <body className="content">Content</body>
+    <div className='App'>
+      <Router>
+        <header className='navbar'>
+          <span className='links'>
+            {links.map((link) => {
+              return <Link to={link.toLowerCase()}>{link}</Link>;
+            })}
+          </span>
+          <span className='name'>HATIBE KABULANTOK</span>
+        </header>
+      </Router>
+      <body className='content'>Content</body>
     </div>
   );
 }
